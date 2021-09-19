e641f4a38d6c8581a1a1b448bb45db0973ac06dc
takhyun12 <takhyun12@gmail.com>
2021-09-19 21:30:23 +0900

    gui 개발시작

--- Frontend/app.py
+++ Frontend/app.py
@@ -17,9 +17,28 @@ st.title('GUI for Vision intelligence research✨')
 """
 
 # body
-st.write('이미지를 대상으로 여러 실험을 편리하게 수행할 수 있습니다.')
+st.write('원하는 이미지를 대상으로 다양한 실험을 편리하게 해볼 수 있습니다.')
 
 uploaded_files = st.file_uploader('Please upload an image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
 for uploaded_file in uploaded_files:
+    col1, col2 = st.columns(2)
     image = Image.open(uploaded_file)
-    st.image(image, use_column_width=True)
+    with col1:
+        st.image(image)
+
+    with col2:
+        st.image(image)
+
+# sidebar
+with st.sidebar:
+    st.header("Layout configuration")
+    with st.form(key="grid_reset"):
+        n_photos = st.slider("Number of images:", min_value=1, max_value=4, value=1)
+        n_cols = st.number_input("Number of columns", min_value=1, max_value=4, value=1)
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.success(n_photos)
+            st.write("slider", n_photos, "checkbox", n_cols)
+
+
+
