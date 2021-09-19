54d5532d3f1cd5e7185ee5d79d7e9d37716e97a3
takhyun12 <takhyun12@gmail.com>
2021-09-19 20:13:19 +0900

    gui 개발시작

--- Frontend/app.py
+++ Frontend/app.py
@@ -3,23 +3,23 @@ from PIL import Image, ImageOps
 
 MAGE_EMOJI_URL = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/259/mage_1f9d9.png"
 
+# browser header
 st.set_page_config(page_title="Vision intelligence GUI", page_icon=MAGE_EMOJI_URL)
-
-# Display header.
 st.markdown("<br>", unsafe_allow_html=True)
+
+# header
 st.image(MAGE_EMOJI_URL, width=80)
-
-
-st.title('Vision intelligence GUI✨')
+st.title('GUI for Vision intelligence research✨')
 """
 [![Github](http://img.shields.io/badge/-Github-black?style=flat-square&logo=github&link=https://github.com/takhyun12?tab=repositories)](https://github.com/takhyun12?tab=repositories)
 &nbsp[![Linkedin](https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/tackhyun-jung-a248941a8/)](https://www.linkedin.com/in/tackhyun-jung-a248941a8)
 &nbsp[![Gmail](https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:takhyun12@gmail.com)](mailto:takhyun12@gmail.com)
 """
 
-st.write('Streamlit! 이 글은 튜토리얼 입니다.')
+# body
+st.write('이미지를 대상으로 여러 실험을 편리하게 수행할 수 있습니다.')
 
-file = st.file_uploader('Please upload an image', type=['jpg', 'jpeg', 'png'])
-if file is not None:
-    image = Image.open(file)
+uploaded_files = st.file_uploader('Please upload an image', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    image = Image.open(uploaded_file)
     st.image(image, use_column_width=True)
