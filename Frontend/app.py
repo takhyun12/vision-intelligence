d07d94dec604d14fe10190156d492c3926071655
takhyun12 <takhyun12@gmail.com>
2021-09-25 19:58:51 +0900

    style transfer ui 편의성 대폭개선

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,3 +1,5 @@
+__version__ = '1.0.0'
+
 import streamlit as st
 from PIL import Image, ImageOps
 
@@ -37,6 +39,9 @@ task_dict = {
 
 # resource => 객체화
 MAGE_EMOJI_URL = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/twitter/259/mage_1f9d9.png"
+GIT_EMOJI_URL = "http://img.shields.io/badge/-Github-black?style=flat-square&logo=github&link=https://github.com/takhyun12?tab=repositories"
+LINKEDIN_EMOJI_URL = "https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/tackhyun-jung-a248941a8/"
+GMAIL_EMOJI_URL = "https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:takhyun12@gmail.com"
 
 # browser header
 st.set_page_config(page_title="Vision intelligence GUI", page_icon=MAGE_EMOJI_URL)
@@ -46,12 +51,13 @@ st.markdown("<br>", unsafe_allow_html=True)
 st.image(MAGE_EMOJI_URL, width=80)
 st.title('GUI for Vision intelligence research✨')
 """
-[![Github](http://img.shields.io/badge/-Github-black?style=flat-square&logo=github&link=https://github.com/takhyun12?tab=repositories)](https://github.com/takhyun12?tab=repositories)
-&nbsp[![Linkedin](https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/tackhyun-jung-a248941a8/)](https://www.linkedin.com/in/tackhyun-jung-a248941a8)
-&nbsp[![Gmail](https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=mailto:takhyun12@gmail.com)](mailto:takhyun12@gmail.com)
+[![Github](GIT_EMOJI_URL)](https://github.com/takhyun12?tab=repositories)
+&nbsp[![Linkedin](LINKEDIN_EMOJI_URL)](https://www.linkedin.com/in/tackhyun-jung-a248941a8)
+&nbsp[![Gmail](GMAIL_EMOJI_URL)](mailto:takhyun12@gmail.com)
 """
 
 body_form = st.form("body_form")
+body_submitted = body_form.form_submit_button("Save")
 
 # sidebar => 객체화
 with st.sidebar:
@@ -106,7 +112,3 @@ with st.sidebar:
     st.error(
         "Found a bug? [Report it](https://github.com/jrieke/traingenerator/issues) 🐛"
     )
-
-
-# body => 객체화
-body_submitted = body_form.form_submit_button("Save")
